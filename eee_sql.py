b8a2775adf54044baaf66ffc2504ed8671b7c3e7
Michael Kunc <mjkunc@gmail.com>
2016-03-31 16:01:15 -0500

    picking up an earlier commit

--- eee_sql.py
+++ eee_sql.py
@@ -16,7 +16,7 @@ class SQL(object):
 		self.profile_id = str(profile_id)
 		self.display_name = display_name
 		self.insert_attrs_b = self.insert_attrs_b(self.eid_instance_id, self.eid_instance_attribute, self.datatype, self.profile_id)
-		self.insert_attrs_tl = self.insert_attrs_tl(self.eid_instance_id, self.eid_instance_attribute, self.display_name)
+		self.insert_attrs_tl = self.insert_attrs_tl(self.eid_instance_id, self.eid_instance_attribute, language_code, self.display_name)
 
 
     def insert_attrs_b(self, eid_instance_id, eid_instance_attribute, datatype, profile_id):
@@ -27,13 +27,26 @@ class SQL(object):
     	return statement
 
 
-    def insert_attrs_tl(self, eid_instance_id ,eid_instance_attribute, display_name):
-    	ebs_language_codes = ('D', 'DK', 'E', 'F', 'NL', 'PT', 'PTB', 'S', 'US', 'ZHS')
+    def insert_attrs_tl(self, eid_instance_id ,eid_instance_attribute, language_code, display_name):
+    	# ebs_language_codes = ('D', 'DK', 'E', 'F', 'NL', 'PT', 'PTB', 'S', 'US', 'ZHS')
     	rem_insert_statement = 'REM INSERTING into APPS.FND_EID_PDR_ATTRS_TL\n'
     	insert_statement = 'Insert into APPS.FND_EID_PDR_ATTRS_TL (EID_INSTANCE_ID,EID_INSTANCE_ATTRIBUTE,LANGUAGE,SOURCE_LANG,DISPLAY_NAME,ATTRIBUTE_DESC,USER_DISPLAY_NAME,USER_ATTRIBUTE_DESC,CREATED_BY,CREATION_DATE,LAST_UPDATED_BY,LAST_UPDATE_DATE,LAST_UPDATE_LOGIN) values'
     	#need to update this string to take the values
     	values = "(" + eid_instance_id + ",'accounting_date','D','US','Accounting Date','Accounting Date','Accounting Date','Accounting Date',0,SYSDATE,0,SYSDATE,0);" 
     	statement = DEFINE_OFF + rem_insert_statement + insert_statement + values
     	return statement
+
+#thinking about separating all tl queries into it's own method. IE -the above will become a single
+#insert statment and the following will handle joining them all together.
+#that means I need to change the structure of the class so that insert_attrs_tl is not a attribute, 
+#but just a method.
+#tests will have to change as well.
+
+
+
+  #   def insert_attrs_tl_all(self):
+		# ebs_language_codes = ('D', 'DK', 'E', 'F', 'NL', 'PT', 'PTB', 'S', 'US', 'ZHS')
+		# for l in ebs_language_codes:
+
 
 
