02026f97da5f7a7b444b20e2d2d1e3848de32c28
Michael Kunc <mjkunc@gmail.com>
2016-05-10 17:23:29 -0500

    xml generator in process

--- edak/xml_generator.py
+++ edak/xml_generator.py
@@ -1,14 +1,25 @@
 class XML(object):
-	METADATA_ID = '<Metadata id="Metadata18" previewAttachmentCharset="ISO-8859-1">'
-	RECORD_ID = '<Record fieldDelimiter="|" name="receiving" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">'
+	METADATA_ID = '<Metadata id="Metadata18" previewAttachmentCharset="ISO-8859-1">\n'
+	RECORD_ID = '<Record fieldDelimiter="|" name="receiving" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">\n'
 	CLOSE_XML = '</Record>\n</Metadata>'
 
-	def __init__(self):
-		self.stuff = 'attributes go here'
+	def __init__(self, field_dictionary):
+		self.fields = self.create_all_fields(field_dictionary)
+		self.file = self.create_xml()
 
-
-	def create_field(self, attribute_name, datatype):
+	def create_single_field(self, attribute_name, datatype):
 		return '<Field name="'+attribute_name+'" type="'+datatype+'"/>'
+
+
+	def create_all_fields(self, field_dictionary):
+		fields = []
+		for key, value in field_dictionary.items():
+			fields.append(self.create_single_field(key, value))
+		return '\n'.join(fields)
+
+
+	def create_xml(self):
+		return XML.METADATA_ID + XML.RECORD_ID + self.fields + XML.CLOSE_XML
 
 
 
--- tests/test_xml_generator.py
+++ tests/test_xml_generator.py
@@ -5,23 +5,28 @@ class XML_TEST(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(XML_TEST):
-		XML_TEST.xml = x.XML()
+		XML_TEST.xml = x.XML({'currency_code': 'string', 'organization_id':'string'})
 
 
 	def test_metadata_id(self):
-		self.assertEqual('<Metadata id="Metadata18" previewAttachmentCharset="ISO-8859-1">', XML_TEST.xml.METADATA_ID)
+		self.assertEqual('<Metadata id="Metadata18" previewAttachmentCharset="ISO-8859-1">\n', XML_TEST.xml.METADATA_ID)
 
 
 	def test_record_id(self):
-		self.assertEqual('<Record fieldDelimiter="|" name="receiving" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">', XML_TEST.xml.RECORD_ID)
+		self.assertEqual('<Record fieldDelimiter="|" name="receiving" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">\n', XML_TEST.xml.RECORD_ID)
 
 
 	def test_close_xml(self):
 		self.assertEqual("</Record>\n</Metadata>", XML_TEST.xml.CLOSE_XML)
 
 
-	def test_create_field(self):
-		self.assertEqual('<Field name="po_header_id" type="string"/>', XML_TEST.xml.create_field('po_header_id', 'string'))
+	def test_create_single_field(self):
+		self.assertEqual('<Field name="po_header_id" type="string"/>', XML_TEST.xml.create_single_field('po_header_id', 'string'))
 
 
+	def test_create_all_fields(self):
+		self.assertEqual('<Field name="organization_id" type="string"/>\n<Field name="currency_code" type="string"/>', XML_TEST.xml.fields)
 
+
+	def test_create_xml(self):
+		self.assertEqual('WHUT', XML_TEST.xml.file)
