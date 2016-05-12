4bd8d00d99b668d93bdb452c9835f14c946e10f1
Michael Kunc <mjkunc@gmail.com>
2016-05-12 10:53:29 -0500

    rename method names

--- edak/xml_generator.py
+++ edak/xml_generator.py
@@ -2,28 +2,30 @@ class XML(object):
 	CLOSE_XML = '\n</Record>\n</Metadata>'
 
 	def __init__(self, field_dictionary, record_name):
-		self.file = self.create_xml(self.create_metadata_id(id(self)), self.create_record_id(record_name), self.create_all_fields(field_dictionary), XML.CLOSE_XML)
+		self.file = self.xml(self.metadata_id(id(self)), self.record_id(record_name), 
+							 self.all_fields(field_dictionary), XML.CLOSE_XML)
 
 
-	def create_metadata_id(self, metadata_id):
+	def metadata_id(self, metadata_id):
 		return  '<Metadata id="{}" previewAttachmentCharset="ISO-8859-1">\n'.format(metadata_id)
 
-	def create_record_id(self, record_name):
+
+	def record_id(self, record_name):
 		return '<Record fieldDelimiter="|" name="{}" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">\n'.format(record_name)
 
 
-	def create_single_field(self, attribute_name, datatype):
+	def single_field(self, attribute_name, datatype):
 		return '<Field name="'+attribute_name+'" type="'+datatype+'"/>'
 
 
-	def create_all_fields(self, field_dictionary):
+	def all_fields(self, field_dictionary):
 		fields = []
 		for key, value in field_dictionary.items():
-			fields.append(self.create_single_field(key, value))
+			fields.append(self.single_field(key, value))
 		return '\n'.join(fields)
 
 
-	def create_xml(self, metadata_id, record_id, fields, CLOSE_XML):
-		return metadata_id + record_id + fields + XML.CLOSE_XML
+	def xml(self, metadata_id, record_id, fields, CLOSE_XML):
+		return metadata_id + record_id + fields + CLOSE_XML
 
 
--- tests/test_xml_generator.py
+++ tests/test_xml_generator.py
@@ -12,24 +12,24 @@ class XML_TEST(unittest.TestCase):
 		XML_TEST.XML_FILE = '<Metadata id="'+str(XML_TEST.metadata_id)+'" previewAttachmentCharset="ISO-8859-1">\n<Record fieldDelimiter="|" name="receiving" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">\n<Field name="organization_id" type="string"/>\n<Field name="currency_code" type="string"/>\n</Record>\n</Metadata>'
 
 
-	def test_create_metadata_id(self):
-		self.assertEqual('<Metadata id="{}" previewAttachmentCharset="ISO-8859-1">\n'.format(XML_TEST.metadata_id), XML_TEST.xml.create_metadata_id(XML_TEST.metadata_id))
+	def test_metadata_id(self):
+		self.assertEqual('<Metadata id="{}" previewAttachmentCharset="ISO-8859-1">\n'.format(XML_TEST.metadata_id), XML_TEST.xml.metadata_id(XML_TEST.metadata_id))
 
 
 	def test_close_xml(self):
 		self.assertEqual("\n</Record>\n</Metadata>", XML_TEST.xml.CLOSE_XML)
 
 
-	def test_create_record_id(self):
-		self.assertEqual('<Record fieldDelimiter="|" name="receiving" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">\n', XML_TEST.xml.create_record_id(XML_TEST.record_name))
+	def test_record_id(self):
+		self.assertEqual('<Record fieldDelimiter="|" name="receiving" previewAttachmentCharset="ISO-8859-1" recordDelimiter="\r\n" recordSize="-1" type="delimited">\n', XML_TEST.xml.record_id(XML_TEST.record_name))
 
-	def test_create_single_field(self):
-		self.assertEqual('<Field name="po_header_id" type="string"/>', XML_TEST.xml.create_single_field('po_header_id', 'string'))
-
-
-	def test_create_all_fields(self):
-		self.assertEqual('<Field name="organization_id" type="string"/>\n<Field name="currency_code" type="string"/>', XML_TEST.xml.create_all_fields(XML_TEST.fields))
+	def test_single_field(self):
+		self.assertEqual('<Field name="po_header_id" type="string"/>', XML_TEST.xml.single_field('po_header_id', 'string'))
 
 
-	def test_create_xml(self):
+	def test_all_fields(self):
+		self.assertEqual('<Field name="organization_id" type="string"/>\n<Field name="currency_code" type="string"/>', XML_TEST.xml.all_fields(XML_TEST.fields))
+
+
+	def test_xml(self):
 		self.assertEqual(XML_TEST.XML_FILE, XML_TEST.xml.file)
