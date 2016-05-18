56a976fd3f350d809b81c1070a593c448821b89d
Michael Kunc <mjkunc@gmail.com>
2016-05-18 15:59:19 -0500

    breaking down the main() method into component parts

--- edak/__main__.py
+++ edak/__main__.py
@@ -6,16 +6,11 @@ import xml_generator
 
 
 def main(args=None):
-	# print 'writing some sweet PLSQL for you........'
-	# reader = reader_writer.Excel_Reader()
-	# sql_writer = reader_writer.Text_Writer('sql.txt')
-	# sql_writer.clear_file()
-	# for r in reader.attribute_data:
-	# 	sql = sql_generator.SQL(*r)
-	# 	sql_writer.save_text(sql.file)
 
-	# print 'beep, boop your PLSQL is ready!'
-	# print '------------------------------------------------------'
+	sql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('sql.txt'))
+
+	eql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('eql.txt'),)
+
 	# print 'now writing some EQL for you..................................'
 	# eql_writer = reader_writer.Text_Writer('eql.txt') 
 	# eql = eql_generator.EQL(reader.attribute_names)
@@ -23,15 +18,32 @@ def main(args=None):
 	# eql_writer.save_text(eql.generate_EQL())
 	# print 'beep, boop your EQL is ready too!'
 
-	print '------------------------------------------------------------'
-	print 'now writing your XML file...........'
-	xml_writer = reader_writer.Text_Writer('xml.txt')
-	xml_writer.clear_file()
-	xml = xml_generator.XML({'test' : 'dict'}, 'receiving')
-	xml_writer.save_text(xml.file)
-	print 'beep, boop your XML is ready as well!'
+	# print '------------------------------------------------------------'
+	# print 'now writing your XML file...........'
+	# xml_writer = reader_writer.Text_Writer('xml.txt')
+	# xml_writer.clear_file()
+	# xml = xml_generator.XML({'test' : 'dict'}, 'receiving')
+	# xml_writer.save_text(xml.file)
+	# print 'beep, boop your XML is ready as well!'
 
 
+def sql_reader_writer(reader, writer):
+	print 'writing some sweet PLSQL for you........'
+	reader = reader
+	writer = writer
+	writer.clear_file()
+	for r in reader.attribute_data:
+		sql = sql_generator.SQL(*r)
+		writer.save_text(sql.file)
+	print 'beep, boop your PLSQL is ready!'
+
+def eql_reader_writer(reader, writer):
+	print 'writing some sweet EQL for you...........'
+	reader = reader
+	writer = writer
+	writer.clear_file()
+	writer.save_text(eql_generator.EQL(reader.attribute_names).generate_EQL())
+	print 'beep, boop, your EQL is ready!'
 
 if __name__ == '__main__':
 	main()
