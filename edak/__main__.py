2bfc499a974cb7807e21736326b133ac07381bae
Michael Kunc <mjkunc@gmail.com>
2016-05-24 14:12:58 -0500

    xml module is now reading from the spreadsheet.

--- edak/__main__.py
+++ edak/__main__.py
@@ -9,15 +9,9 @@ def main(args=None):
 
 	sql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('sql.txt'))
 
-	eql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('eql.txt'),)
+	eql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('eql.txt'))
 
-	# print '------------------------------------------------------------'
-	# print 'now writing your XML file...........'
-	# xml_writer = reader_writer.Text_Writer('xml.txt')
-	# xml_writer.clear_file()
-	# xml = xml_generator.XML({'test' : 'dict'}, 'receiving')
-	# xml_writer.save_text(xml.file)
-	# print 'beep, boop your XML is ready as well!'
+	xml_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('xml.txt'))
 
 
 def sql_reader_writer(reader, writer):
@@ -37,6 +31,15 @@ def eql_reader_writer(reader, writer):
 	writer.clear_file()
 	writer.save_text(eql_generator.EQL(reader.attribute_names).generate_EQL())
 	print 'beep, boop, your EQL is ready!'
+
+def xml_reader_writer(reader, writer):
+	print 'writing some sweet XML for you'
+	reader = reader
+	writer = writer
+	writer.clear_file()
+	writer.save_text(xml_generator.XML(reader.attribute_dict, 'metadata1').file)
+	print 'beep, boop your XML is ready'
+
 
 if __name__ == '__main__':
 	main()
--- edak/reader_writer.py
+++ edak/reader_writer.py
@@ -1,5 +1,6 @@
 import openpyxl
 
+
 class Excel_Reader(object):
 
 	def __init__(self):
@@ -7,11 +8,22 @@ class Excel_Reader(object):
 		sheet = wb.get_sheet_by_name('endeca_attributes')
 		highest_row = str(sheet.get_highest_row())
 		self.attribute_data = [[c.value for c in rowOfCellObjects] for rowOfCellObjects in sheet['A2':'E'+highest_row]]
+
+#UUUUGHHHHHHHHHHHHH
 # this puts the quick and dirty into quick and dirty
+
 		self.attribute_names = []
 		for col in sheet.columns[1]:
 			self.attribute_names.append(col.value)
 		del self.attribute_names[0]
+
+####UGH SOOOOO UGLY
+		xml_data = []
+		for rowOfCellObjects in sheet['B2':'C'+highest_row]:
+			for col in rowOfCellObjects:
+				xml_data.append(col.value)
+		self.attribute_dict = {item : xml_data[index+1] for index, item in enumerate(xml_data) if index % 2 == 0}
+
 
 class Text_Writer(object):
 
