3d9bd0c878ef6b8921aff662ab32cfa6b6768dca
Michael Kunc <mjkunc@gmail.com>
2016-06-03 06:19:22 -0700

    simplifying main method

--- edak/__main__.py
+++ edak/__main__.py
@@ -7,35 +7,29 @@ import xml_generator
 
 def main(args=None):
 
-	# sql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('sql.txt'))
+	sql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('sql.txt'))
 
-	# eql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('eql.txt'))
+	eql_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('eql.txt'))
 
 	xml_reader_writer(reader_writer.Excel_Reader(), reader_writer.Text_Writer('xml.txt'))
 
 
-# def sql_reader_writer(reader, writer):
-# 	print 'writing some sweet PLSQL for you........'
-# 	reader = reader
-# 	writer = writer
-# 	writer.clear_file()
-# 	for r in reader.attribute_data:
-# 		sql = sql_generator.SQL(*r)
-# 		writer.save_text(sql.file)
-# 	print 'beep, boop your PLSQL is ready!'
+def sql_reader_writer(reader, writer):
+	print 'writing some sweet PLSQL for you........'
+	writer.clear_file()
+	for r in reader.attribute_data:
+		sql = sql_generator.SQL(*r)
+		writer.save_text(sql.file)
+	print 'beep, boop your PLSQL is ready!'
 
-# def eql_reader_writer(reader, writer):
-# 	print 'writing some sweet EQL for you...........'
-# 	reader = reader
-# 	writer = writer
-# 	writer.clear_file()
-# 	writer.save_text(eql_generator.EQL(reader.attribute_names).generate_EQL())
-# 	print 'beep, boop, your EQL is ready!'
+def eql_reader_writer(reader, writer):
+	print 'writing some sweet EQL for you...........'
+	writer.clear_file()
+	writer.save_text(eql_generator.EQL(reader.attribute_names).generate_EQL())
+	print 'beep, boop, your EQL is ready!'
 
 def xml_reader_writer(reader, writer):
 	print 'writing some sweet XML for you'
-	reader = reader
-	writer = writer
 	writer.clear_file()
 	writer.save_text(xml_generator.XML(reader.xml_attributes, 'metadata1').file)
 	print 'beep, boop your XML is ready'
