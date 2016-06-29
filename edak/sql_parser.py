bf3f093c8d335e7d245dfa9ba2ededd91af43ca2
michaelkunc <mjkunc@gmail.com>
2016-06-28 18:15:39 -0700

    grabbing local changes

--- edak/sql_parser.py
+++ edak/sql_parser.py
@@ -1,47 +1,41 @@
 import os
 import re
 
+
 class SQL_PARSER(object):
 
+    def __init__(self, sql_file_name):
+        self.sql_file_name = sql_file_name
 
-	def __init__(self, sql_file_name):
-		self.sql_file_name = sql_file_name
+    def check_for_file(self):
+        return os.path.isfile(self.sql_file_name)
 
+    def open_file(self):
+        with open(self.sql_file_name) as f:
+            lines = f.read().splitlines()
+            return lines
 
-	def check_for_file(self):
-		return os.path.isfile(self.sql_file_name)
+    def find_from_index(self, sql_lines):
+        for index, line in enumerate(sql_lines):
+            if 'FROM' in line:
+                return index
 
+    def get_selected_columns(self, sql_lines, index_of_from):
+        return [i.strip().replace(',', '') for i in sql_lines[1:index_of_from] if len(i) > 0]
 
-	def open_file(self):
-		with open(self.sql_file_name) as f:
-			lines = f.read().splitlines()
-			return lines
+    def remove_table_names(self, selected_column_lines):
+        return [item[item.index('.') + 1:] for item in selected_column_lines]
 
-	def find_from_index(self, sql_lines):
-		for index, line in enumerate(sql_lines):
-			if 'FROM' in line:
-				return index
+    def format_column_aliases(self, selected_column_lines):
+        return [item[item.index('AS') + 3:] if 'AS' in item else item for item in selected_column_lines]
 
-
-	def get_selected_columns(self,sql_lines, index_of_from):
-		return [i.strip().replace(',','') for i in sql_lines[1:index_of_from] if len(i) > 0]
-
-
-	def remove_table_names(self, selected_column_lines):
-		return [item[item.index('.') + 1:] for item in selected_column_lines]
-
-
-	def format_column_aliases(self, selected_column_lines):
-		return [item[item.index('AS') + 3:] if 'AS' in item else item for item in selected_column_lines]
-
-
-	def parse_sql_file(self):
-		if self.check_for_file():
-			raw_sql_lines = self.open_file()
-			from_index = self.find_from_index(raw_sql_lines)
-			sql_lines = self.get_selected_columns(raw_sql_lines, from_index)
-			sql_lines = self.remove_table_names(sql_lines)
-			sql_lines = self.format_column_aliases(sql_lines)
-			return sql_lines
-		else:
-			print 'file is not found'
+    def parse_sql_file(self):
+        if self.check_for_file():
+            raw_sql_lines = self.open_file()
+            from_index = self.find_from_index(raw_sql_lines)
+            sql_lines = self.get_selected_columns(raw_sql_lines, from_index)
+            sql_lines = self.remove_table_names(sql_lines)
+            sql_lines = self.format_column_aliases(sql_lines)
+            return sql_lines
+        else:
+            print 'file is not found'
--- tests/test_sql_parser.py
+++ tests/test_sql_parser.py
@@ -1,38 +1,38 @@
 import unittest
 from edak import sql_parser as sp
 
+
 class SQL_PARSER_TEST(unittest.TestCase):
 
-	@classmethod
-	def setUpClass(SQL_PARSER_TEST):
-		SQL_PARSER_TEST.instance = sp.SQL_PARSER('QTO_308.sql')
-		SQL_PARSER_TEST.sql_lines = SQL_PARSER_TEST.instance.open_file()
-		SQL_PARSER_TEST.column_headers = SQL_PARSER_TEST.instance.get_selected_columns(SQL_PARSER_TEST.sql_lines, 14)
+    @classmethod
+    def setUpClass(SQL_PARSER_TEST):
+        SQL_PARSER_TEST.instance = sp.SQL_PARSER('QTO_308.sql')
+        SQL_PARSER_TEST.sql_lines = SQL_PARSER_TEST.instance.open_file()
+        SQL_PARSER_TEST.column_headers = SQL_PARSER_TEST.instance.get_selected_columns(
+            SQL_PARSER_TEST.sql_lines, 14)
 
+    def test_check_for_file(self):
+        self.assertTrue(SQL_PARSER_TEST.instance.check_for_file())
 
-	def test_check_for_file(self):
-		self.assertTrue(SQL_PARSER_TEST.instance.check_for_file())
+    def test_open_file(self):
+        self.assertEqual('SELECT', SQL_PARSER_TEST.instance.open_file()[0])
 
+    def test_find_from_index(self):
+        self.assertEqual(14, SQL_PARSER_TEST.instance.find_from_index(
+            SQL_PARSER_TEST.sql_lines))
 
-	def test_open_file(self):
-		self.assertEqual('SELECT', SQL_PARSER_TEST.instance.open_file()[0])
+    def test_get_select_columns(self):
+        self.assertEqual(['RCTA.CUSTOMER_TRX_ID', 'RCTA.PURCHASE_ORDER', 'RCTLGDA.GL_DATE AS dt_revenue', 'RCTLA.INTERFACE_LINE_ATTRIBUTE1 AS SALES_ORDER', 'GCC.SEGMENT5 AS Wwapc', 'OOHA.ATTRIBUTE1 AS SALES_OFFICE', 'MSIB.SEGMENT1 AS PART_NUMBER'],
+                         SQL_PARSER_TEST.instance.get_selected_columns(SQL_PARSER_TEST.sql_lines, 14))
 
+    def test_remove_table_names(self):
+        self.assertEqual(['CUSTOMER_TRX_ID', 'PURCHASE_ORDER', 'GL_DATE AS dt_revenue', 'INTERFACE_LINE_ATTRIBUTE1 AS SALES_ORDER', 'SEGMENT5 AS Wwapc',
+                          'ATTRIBUTE1 AS SALES_OFFICE', 'SEGMENT1 AS PART_NUMBER'], SQL_PARSER_TEST.instance.remove_table_names(SQL_PARSER_TEST.column_headers))
 
-	def test_find_from_index(self):
-		self.assertEqual(14, SQL_PARSER_TEST.instance.find_from_index(SQL_PARSER_TEST.sql_lines))
+    def test_format_column_aliases(self):
+        self.assertEqual(['RCTA.CUSTOMER_TRX_ID', 'PURCHASE_ORDER', 'dt_revenue', 'SALES_ORDER', 'Wwapc', 'SALES_OFFICE',
+                          'PART_NUMBER'], SQL_PARSER_TEST.instance.format_column_aliases(SQL_PARSER_TEST.column_headers))
 
-
-	def test_get_select_columns(self):
-		self.assertEqual(['RCTA.CUSTOMER_TRX_ID', 'RCTA.PURCHASE_ORDER','RCTLGDA.GL_DATE AS dt_revenue','RCTLA.INTERFACE_LINE_ATTRIBUTE1 AS SALES_ORDER','GCC.SEGMENT5 AS Wwapc', 'OOHA.ATTRIBUTE1 AS SALES_OFFICE', 'MSIB.SEGMENT1 AS PART_NUMBER'],
-			SQL_PARSER_TEST.instance.get_selected_columns(SQL_PARSER_TEST.sql_lines, 14))
-
-	def test_remove_table_names(self):
-		self.assertEqual(['CUSTOMER_TRX_ID','PURCHASE_ORDER','GL_DATE AS dt_revenue','INTERFACE_LINE_ATTRIBUTE1 AS SALES_ORDER','SEGMENT5 AS Wwapc','ATTRIBUTE1 AS SALES_OFFICE', 'SEGMENT1 AS PART_NUMBER'], SQL_PARSER_TEST.instance.remove_table_names(SQL_PARSER_TEST.column_headers))
-
-
-	def test_format_column_aliases(self):
-		self.assertEqual(['RCTA.CUSTOMER_TRX_ID','PURCHASE_ORDER','dt_revenue','SALES_ORDER','Wwapc','SALES_OFFICE', 'PART_NUMBER'],SQL_PARSER_TEST.instance.format_column_aliases(SQL_PARSER_TEST.column_headers))
-
-
-	def test_parse_sql_lines(self):
-		self.assertEqual(['CUSTOMER_TRX_ID', 'PURCHASE_ORDER', 'dt_revenue', 'SALES_ORDER', 'Wwapc', 'SALES_OFFICE', 'PART_NUMBER'], SQL_PARSER_TEST.instance.parse_sql_file())
+    def test_parse_sql_lines(self):
+        self.assertEqual(['CUSTOMER_TRX_ID', 'PURCHASE_ORDER', 'dt_revenue', 'SALES_ORDER',
+                          'Wwapc', 'SALES_OFFICE', 'PART_NUMBER'], SQL_PARSER_TEST.instance.parse_sql_file())
