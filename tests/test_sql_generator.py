7f35403e7f79979dacca05cea958fac46a6de64c
Michael Kunc <mjkunc@gmail.com>
2016-07-26 13:22:36 -0500

    all slices removed from test suite

--- tests/test_sql_generator.py
+++ tests/test_sql_generator.py
@@ -9,9 +9,7 @@ class SQL(unittest.TestCase):
         SQL.sql = s.SQL(204, 'accounting_period',
                              'mdex:string', 1, 'Accounting Period')
         SQL.instance_id = '204'
-        SQL.update_attr_groups_set = slice(48, 195)
         SQL.column_name_string = ' (EID_INSTANCE_ID,EID_INSTANCE_ATTRIBUTE,LANGUAGE,SOURCE_LANG,DISPLAY_NAME,ATTRIBUTE_DESC,USER_DISPLAY_NAME,USER_ATTRIBUTE_DESC,CREATED_BY,CREATION_DATE,LAST_UPDATED_BY,LAST_UPDATE_DATE,LAST_UPDATE_LOGIN) values'
-
 
     def test_insert_attrs_b_define_clause(self):
         self.assertEqual('SET DEFINE OFF;\n', SQL.sql.insert_attrs_b[0:16])
@@ -41,20 +39,21 @@ class SQL(unittest.TestCase):
                          insert_statement[242:427])
 
     def test_insert_attrs_tl_all_define_clause(self):
-        self.assertEqual('SET DEFINE OFF;\n', SQL.sql.insert_attrs_tl_all[0:16])
+        self.assertEqual('SET DEFINE OFF;\n',
+                         SQL.sql.insert_attrs_tl_all[0:16])
 
     def test_insert_attrs_tl_all_rem_clause(self):
         insert_statement = SQL.sql.insert_attrs_tl_all
         self.assertEqual('REM INSERTING into APPS.FND_EID_PDR_ATTRS_TL\n',
                          insert_statement[16:61])
 
-#     def test_insert_attrs_tl_length(self):
-#         insert_statement = SQL.sql.insert_attrs_tl_all
-#         self.assertEqual(3957, len(SQL.sql.insert_attrs_tl_all))
+    def test_insert_attrs_tl_length(self):
+        insert_statement = SQL.sql.insert_attrs_tl_all
+        self.assertEqual(3957, len(SQL.sql.insert_attrs_tl_all))
 
-#     def test_insert_attrs_tl_all_commit(self):
-#         insert_statement = SQL.sql.insert_attrs_tl_all
-#         self.assertEqual('COMMIT;', SQL.sql.insert_attrs_tl_all[-7:])
+    def test_insert_attrs_tl_all_commit(self):
+        insert_statement = SQL.sql.insert_attrs_tl_all
+        self.assertEqual('COMMIT;', SQL.sql.insert_attrs_tl_all[-7:])
 
     def test_insert_attrs_groups_set_define_clause(self):
         self.assertEqual('SET DEFINE OFF;\n', SQL.sql.insert_attr_groups[0:16])
@@ -67,30 +66,30 @@ class SQL(unittest.TestCase):
         self.assertEqual("values ( 204,'Categories','accounting_period',1,1,'MSI','2.3','N',0,0,SYSDATE,0,SYSDATE,0);",
                          SQL.sql.insert_attr_groups[378:469])
 
-#     def test_update_attr_groups(self):
-#         self.assertEqual("SET EID_INSTANCE_GROUP_ATTR_SEQ = 1, EID_INST_GROUP_ATTR_USER_SEQ = 1 WHERE EID_INSTANCE_ID = 204 AND EID_INSTANCE_ATTRIBUTE = 'accounting_period';",
-#                          SQL.sql.update_attr_groups[SQL.update_attr_groups_set])
+    def test_update_attr_groups(self):
+        self.assertEqual("SET EID_INSTANCE_GROUP_ATTR_SEQ = 1, EID_INST_GROUP_ATTR_USER_SEQ = 1 WHERE EID_INSTANCE_ID = 204 AND EID_INSTANCE_ATTRIBUTE = 'accounting_period';",
+                         SQL.sql.update_attr_groups[48:195])
 
-#     def test_create_insert_statement(self):
-#         column_headers = ['EID_INSTANCE_ID',
-#                           'EID_INSTANCE_ATTRIBUTE', 'ENDECA_DATATYPE']
-#         self.assertEqual('Insert into APPS.FND_EID_PDR_ATTRS_B (EID_INSTANCE_ID,EID_INSTANCE_ATTRIBUTE,ENDECA_DATATYPE)\n',
-#                          SQL.sql.create_insert_statement(s.SQL.ATTRS_B, column_headers))
+    def test_create_insert_statement(self):
+        column_headers = ['EID_INSTANCE_ID',
+                          'EID_INSTANCE_ATTRIBUTE', 'ENDECA_DATATYPE']
+        self.assertEqual('Insert into APPS.FND_EID_PDR_ATTRS_B (EID_INSTANCE_ID,EID_INSTANCE_ATTRIBUTE,ENDECA_DATATYPE)\n',
+SQL.sql.create_insert_statement(s.SQL.ATTRS_B, column_headers))
 
-#     def test_concat_schema_table(self):
-#         self.assertEqual('APPS.FND_EID_ATTRS_B', SQL.sql.concat_schema_table(
-#             'APPS', 'FND_EID_ATTRS_B'))
+    def test_concat_schema_table(self):
+        self.assertEqual('APPS.FND_EID_ATTRS_B', SQL.sql.concat_schema_table(
+            'APPS', 'FND_EID_ATTRS_B'))
 
-#     def test_create_column_name_string(self):
-#         column_headers = ['EID_INSTANCE_ID', 'EID_INSTANCE_GROUP',
-#                           'EID_INSTANCE_ATTRIBUTE', 'EID_INSTANCE_GROUP_ATTR_SEQ']
-#         self.assertEqual(' (EID_INSTANCE_ID,EID_INSTANCE_GROUP,EID_INSTANCE_ATTRIBUTE,EID_INSTANCE_GROUP_ATTR_SEQ)\n',
-#                          SQL.sql.create_column_name_string(*column_headers))
+    def test_create_column_name_string(self):
+        column_headers = ['EID_INSTANCE_ID', 'EID_INSTANCE_GROUP',
+                          'EID_INSTANCE_ATTRIBUTE', 'EID_INSTANCE_GROUP_ATTR_SEQ']
+        self.assertEqual(' (EID_INSTANCE_ID,EID_INSTANCE_GROUP,EID_INSTANCE_ATTRIBUTE,EID_INSTANCE_GROUP_ATTR_SEQ)\n',
+                         SQL.sql.create_column_name_string(*column_headers))
 
-#     def test_create_values_string(self):
-#         values = ['204', 'accounting_period', 'mdex:string', '4']
-#         self.assertEqual("values ( 204,'accounting_period','mdex:string',4);",
-#                          SQL.sql.create_values_string(*values))
+    def test_create_values_string(self):
+        values = ['204', 'accounting_period', 'mdex:string', '4']
+        self.assertEqual("values ( 204,'accounting_period','mdex:string',4);",
+                         SQL.sql.create_values_string(*values))
 
 
 if __name__ == '__main__':
