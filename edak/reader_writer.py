6a4b3156db09f7ac68c0a369ad2bd821e5279649
Michael Kunc <mjkunc@gmail.com>
2016-08-09 07:47:31 -0700

    slight improvement to reader_writer

--- edak/reader_writer.py
+++ edak/reader_writer.py
@@ -3,58 +3,51 @@ import openpyxl
 
 class Excel_Reader(object):
 
-	def __init__(self, file_name):
-		wb = openpyxl.load_workbook(file_name)
-		sheet = wb.get_sheet_by_name('endeca_attributes')
-		highest_row = str(sheet.get_highest_row())
-		self.attribute_data = [[c.value for c in rowOfCellObjects] for rowOfCellObjects in sheet['A2':'E'+highest_row]]
+    def __init__(self, file_name):
+        wb = openpyxl.load_workbook(file_name)
+        sheet = wb.get_sheet_by_name('endeca_attributes')
+        highest_row = str(sheet.get_highest_row())
+        self.attribute_data = [[c.value for c in rowOfCellObjects]
+                               for rowOfCellObjects in sheet['A2':'E' + highest_row]]
 
-#UUUUGHHHHHHHHHHHHH
-# this puts the quick and dirty into quick and dirty
+        self.eql_attributes = [row.value for row in sheet.columns[1]]
+        del self.eql_attributes[0]
 
-		self.eql_attributes = []
-		for col in sheet.columns[1]:
-			self.eql_attributes.append(col.value)
-		del self.eql_attributes[0]
+        self.xml_attributes = [[c.value for c in rowOfCellObjects]
+                               for rowOfCellObjects in sheet['B2':'C' + highest_row]]
 
-		self.xml_attributes = [[c.value for c in rowOfCellObjects] for rowOfCellObjects in sheet['B2':'C'+highest_row]]
 
 class Excel_Writer(object):
 
-	def __init__(self, file_name, columns_and_datatype):
-		self.file_name = file_name
-		self.columns_and_datatype = columns_and_datatype
-		self.create_excel_file()
+    def __init__(self, file_name, columns_and_datatype):
+        self.file_name = file_name
+        self.columns_and_datatype = columns_and_datatype
+        self.create_excel_file()
 
-#naive implementation
-	def create_excel_file(self):
-		wb = openpyxl.Workbook()
-		ws = wb.active
-		ws.title = 'endeca_attributes'
-		ws['A1'].value ='eid_instance_id'
-		ws['B1'].value = 'eid_instance_attribute'
-		ws['C1'].value = 'datatype'
-		ws['D1'].value = 'profile_id'
-		ws['E1'].value = 'display_name'
-		for i, e in enumerate(self.columns_and_datatype):
-			ws.cell(row=i+2, column=2).value = e[0]
-			ws.cell(row=i+2, column=3).value = e[1]
-		wb.save(self.file_name)
+# naive implementation
+    def create_excel_file(self):
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = 'endeca_attributes'
+        ws['A1'].value = 'eid_instance_id'
+        ws['B1'].value = 'eid_instance_attribute'
+        ws['C1'].value = 'datatype'
+        ws['D1'].value = 'profile_id'
+        ws['E1'].value = 'display_name'
+        for i, e in enumerate(self.columns_and_datatype):
+            ws.cell(row=i + 2, column=2).value = e[0]
+            ws.cell(row=i + 2, column=3).value = e[1]
+        wb.save(self.file_name)
 
 
 class Text_Writer(object):
 
-	def __init__(self, file):
-		self.file = file
+    def __init__(self, file):
+        self.file = file
 
+    def clear_file(self):
+        open(self.file, 'w').close()
 
-	def clear_file(self):
-		open(self.file, 'w').close()
-
-
-	def save_text(self, text):
-		with open(self.file, 'a') as f:
-			f.write(text)
-
-
-
+    def save_text(self, text):
+        with open(self.file, 'a') as f:
+            f.write(text)
