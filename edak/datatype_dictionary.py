67e251c04db5d69e1d3bb439970a196ab12e578d
Michael Kunc <mjkunc@gmail.com>
2016-08-23 18:49:36 -0500

    additions to the datatype_dictionary

--- edak/datatype_dictionary.py
+++ edak/datatype_dictionary.py
@@ -5,7 +5,9 @@ ORACLE_COLUMNS_TO_ENDECA = {'FULFILLMENT_DATE':'mdex:dateTime', 'GL_DATE' : 'mde
 				 'ACTUAL_ARRIVAL_DATE' : 'mdex:dateTime', 'ULTIMATE_DROPOFF_DATE': 'mdex:dateTime', 'PROMISED_DATE':'mdex:dateTime',
 				 'LEAD_TIME':'mdex:int', 'POSTPROCESSING_LEAD_TIME' : 'mdex:int', 'FULL_LEAD_TIME' : 'mdex:int', 
 				 'PREPROCESSING_LEAD_TIME' : 'mdex:int', 'DELIVERY_LEAD_TIME':'mdex:int', 'SCHEDULE_ARRIVAL_DATE' : 'mdex:dateTime',
-				 'TOTAL_UNSHIPPED_QTY' : 'mdex:int', 'CONVERSION_RATE' : 'mdex:double', 'BOOKED_DATE' : 'mdex:dateTime'}
+				 'TOTAL_UNSHIPPED_QTY' : 'mdex:int', 'CONVERSION_RATE' : 'mdex:double', 'BOOKED_DATE' : 'mdex:dateTime',
+				 'OFFER_START_DATE' :'mdex:dateTime', 'OFFER_END_DATE': 'mdex:dateTime', 'ULTILIZED_AMOUNT' : 'mdex:double',
+				 'COMMITTED_AMOUNT' : 'mdex:double','EARNED_AMOUNT' : 'mdex:double'}
 
 ENDECA_TO_XML = {'mdex:double': 'number', 'mdex:string': 'string', 'mdex:boolean': 'boolean',
                             'mdex:dateTime': 'date', 'mdex:int': 'integer',
